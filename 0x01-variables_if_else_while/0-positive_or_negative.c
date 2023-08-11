87f7cf350b6030b3eb5cd5be66ac6e2b2e9e943d
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-11 16:30:40 -0700

    fixed trailing white space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - Entry point. prints if n is positive, negative, or equal to zero. 
+ * main - Entry point. prints if n is positive, negative, or equal to zero
  *
  * Return: 0 if successful.
  */
