7490d547723324898ba25f15f15f50afab2b8335
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-11 16:35:01 -0700

    fixed an oopsie

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%i is positive");
+		printf("%i is positive", n);
 	else if (n == 0)
-		printf("%i is zero");
+		printf("%i is zero", n);
 	else
-		printf("%i is negative");
+		printf("%i is negative", n);
 	return (0);
 }
