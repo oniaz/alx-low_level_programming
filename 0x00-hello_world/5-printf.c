7082043dc45d63d64c95f3510cde0b72a04878b2
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-10 18:13:47 -0700

    betty ahhhhh

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints the desired output
+ *
+ * Return: 0 for success
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
