04d8e5a906d8c55beb9605e3da0d1dccd7093c7b
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-11 18:22:25 -0700

    putchar() 48 -> 57 ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the digits 0-9
+ *
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	
+
+	putchar('\n');
+
+	return (0);
+}
