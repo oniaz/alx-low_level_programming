08c759a5dca6b849aab7485496762b9b9d349598
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-11 18:50:12 -0700

    done el7

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the digits 0-9 with , and space
+ *
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(32);
+		}
+	}
+
+	return (0);
+}
