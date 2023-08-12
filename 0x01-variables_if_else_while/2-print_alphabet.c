d5adf37658611305d6691a2d249b9a626f41f54f
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-11 17:34:35 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	char c;
+
+	for (int i = 0; i < 26; i++)
+	{
+		c = (char) (i + 97);
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
