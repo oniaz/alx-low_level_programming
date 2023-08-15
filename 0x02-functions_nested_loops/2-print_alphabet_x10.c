26459b9805a89780a8b0d327786316fd66ad23fb
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-15 16:23:31 -0700

    made a function that loops 10 times and uses the function from the prev task

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - loops 10 times with print_alphabet()
+ */
+void print_alphabet_x10(void)
+{
+	int n = 10;
+
+	while (n--)
+	{
+		print_alphabet();
+	}
+}
