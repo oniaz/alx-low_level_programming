41715ae77f2dfaaac1821636520ec4060f81cf95
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-15 19:14:30 -0700

    wtf was that soundtrack

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,17 +1,12 @@
-#include <limits.h>
 #include "main.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int r;
-
-	r = print_last_digit(INT_MIN);
-	_putchar('0' + r);
-	_putchar('\n');
-	return (0);
+    jack_bauer();
+    return (0);
 }
