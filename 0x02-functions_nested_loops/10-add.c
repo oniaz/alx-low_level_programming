28f597c039099e22e476390b8678904fc8bf053b
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-15 21:06:24 -0700

    simple addition function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds 2 numbers
+ * @n: first number
+ * @m: second number
+ * Return: n the sum of the 2 numbers
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
