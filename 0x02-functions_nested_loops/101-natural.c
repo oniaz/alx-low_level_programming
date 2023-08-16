4247623fe341560122983ff55de2c0ba0f0cb199
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-16 16:02:53 -0700

    im so slow today

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * natural - prints the sum of all the multiples of 3 or 5
+ * below 1024 (excluded) followed by a new line
+ * Return: sum
+ */
+int natural(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i += 3)
+	{
+		if (i + 3 < 1024)
+			sum += i + 3;
+		else
+			break;
+	}
+	for (i = 0; i < 1024; i += 5)
+	{
+		if (i + 5 < 1024)
+			sum += i + 5;
+		else
+			break;
+	}
+	printf("%d\n", sum);
+
+	return (sum);
+}
+/**
+ * main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	natural();
+
+	return (0);
+}
