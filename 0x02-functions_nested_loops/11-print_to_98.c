7247be57efe88c490385180ff6e04c31e0a2f3e0
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-15 21:30:49 -0700

    ._.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - print natural numbers from n to 98,
+ * separated by a comma, followed by a space, followed by a new line
+ */
+void print_to_98(int n)
+{
+	int flag = 1;
+
+	while (n <= 98)
+	{
+		if (flag == 1)
+			flag = 0;
+		else
+		{
+			printf(", ");
+		}
+		printf("%d", n);
+		n++;
+	}
+}
