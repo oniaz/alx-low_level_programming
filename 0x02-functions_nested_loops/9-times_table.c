2b0d17ad0a0fefb0289f3e491365cbe172de1c10
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-15 19:57:17 -0700

    hsdsbghgbkjdf

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table.
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j= 0; j <= 9; j++)
+		{
+			if ((i * j) / 10 == 0)
+				_putchar(' ');
+			
+			else 
+				_putchar(((i*j) / 10) + '0');
+			_putchar(((i*j) % 10) + '0');
+			if (j == 9)
+			{
+		//		_putchar('$');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
