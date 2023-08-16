2102044ba48d7ab0433fc7cee8902ce2425f6815
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-15 20:54:35 -0700

    obv there's a smarter way to do it but I'm not feeling smart atm

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,19 +12,27 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if (((i * j) / 10 == 0) && (j != 0))
-				_putchar(' ');
-			else
-				_putchar(((i * j) / 10) + '0');
-			_putchar(((i * j) % 10) + '0');
-			if (j == 9)
+			if (j == 0)
 			{
-				_putchar('\n');
-			}
-			else
-			{
+				_putchar('0');
 				_putchar(',');
 				_putchar(' ');
+			}
+			else 
+			{	if ((i * j) / 10 == 0)
+					_putchar(' ');
+				else
+					_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
+				if (j == 9)
+				{
+					_putchar('\n');
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
 		}
 	}
