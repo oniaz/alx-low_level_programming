c3085045b0a516fc66b54ddc8e9f249d38f5f7b9
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-15 19:59:48 -0700

    h

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,17 +10,15 @@ void times_table(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j= 0; j <= 9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			if ((i * j) / 10 == 0)
 				_putchar(' ');
-			
-			else 
-				_putchar(((i*j) / 10) + '0');
-			_putchar(((i*j) % 10) + '0');
+			else
+				_putchar(((i * j) / 10) + '0');
+			_putchar(((i * j) % 10) + '0');
 			if (j == 9)
 			{
-		//		_putchar('$');
 				_putchar('\n');
 			}
 			else
