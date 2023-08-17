ee8d005fad631b570ed923351390176de95461ba
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-17 11:24:11 -0700

    fixed function return type

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
-print_remaining_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif /* MAIN_H */
