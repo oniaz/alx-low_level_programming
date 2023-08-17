3de50c5234641cfa5af559dc4b27c38c21a6ef95
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-17 11:17:47 -0700

    last task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 
-int largest_number(int a, int b, int c);
+print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
