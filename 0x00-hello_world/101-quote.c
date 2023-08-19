9162370c6266ee2d9d99d5032e956603815d6ba7
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-19 16:38:44 -0700

    write(File Descriptor - like 1 or 2,BUFFER -> pointer to data, count size_t in bytes) i used 2 for standard output - char *str -> pointer to store char array address in str.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Return: 1 for success
+ */
+
+int main(void)
+{
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, strlen(str));
+
+	return (1);
+}
