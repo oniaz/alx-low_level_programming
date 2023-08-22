8e04aa281571435f707998643c07c0f27ab42533
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-21 18:57:29 -0700

    ye

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,13 +8,13 @@
  */
 void puts2(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str += 2;
+	int i 0;
 
-		if (*str == '\0')
-			break;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
 }
