f8c8af879fca2798b9f1c6cd4b7c9dbef893faf8
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-28 16:29:09 -0700

    1st task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_memset- encodes a string using rot13
+ * @s: pointer to memory area
+ * @b: const byter that wil fill the memory area
+ * @n: number of bytes to be filled
+ * Return: pointer to the string after rot13 encoding
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n > 0)
+	{
+		*ptr = b;
+		ptr++;
+		n--;
+	}
+
+	return (s);
