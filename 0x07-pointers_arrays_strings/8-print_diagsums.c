4a1f0af6023ca82f9c84205e27b3241456dfa37b
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-28 20:50:20 -0700

    an hour after the deadline but it's okay because im cramping

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - prints the sum of the 2 diagonals of a square matrix
+ * @a: pointer to the first element in the matrix
+ * @size: size of the square matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	unsigned long long sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+
+	printf("%llu, %llu\n", sum1, sum2);
+}
