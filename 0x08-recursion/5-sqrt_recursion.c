ec0fcb291ab5c1eb9138b6d1320a01fa608586eb
oniaz <32391049+oniaz@users.noreply.github.com>
2023-08-30 18:38:31 -0700

    purrfkt skware

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * purrfkt - checks if a number is a prime number
+ * @n: number to be tested
+ * @x: divisor
+ * Return: 1 if prime otherwise 0
+ */
+int purrfkt(int n, int x)
+{
+	if (x > n / 2)
+		return (-1);
+
+	if (x == 1)
+		return (1);
+
+	if (n == x * x)
+		return (x);
+
+	else
+		return (purrfkt(n, x + 1));
+}
+
+/**
+ * _sqrt_recursion - finds out if there's a natural square root for the number
+ * then prints it
+ * @n: numeber to be tested
+ * Return: the natural square root, if it's non existant it returns -1
+ */
+int _sqrt_recursion(int n)
+{
+	if (n <= 0)
+		return (-1);
+
+	if (n == 1)
+		return (1);
+
+	else
+		return (purrfkt(n, 2));
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,5 +10,6 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int meow(int n, int x);
 int is_prime_number(int n);
+int purrfkt(int n, int x);
 
 #endif
