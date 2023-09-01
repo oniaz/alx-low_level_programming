7a61c042906285ba9fc3b63d375133045262ab7b
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-01 16:11:33 -0700

    same code from the docs

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "main.h"
+
+/**
+ * main - prints all arguments it receives, each in a separate line.
+ * @argc: count of the arguments supplied to the program
+ * @argv: array of pointers to the arguments
+ * Return: On success 0
+ */
+int main(int argc, char **argv)
+{
+	while (argc--)
+		printf("%s\n", *argv++);
+
+	return (0);
+}	
