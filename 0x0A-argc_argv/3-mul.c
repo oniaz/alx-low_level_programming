ae0a546ad37e074998688ef2621cdb2e90296478
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-01 17:06:05 -0700

    4th task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -15,7 +16,7 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-	printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }
