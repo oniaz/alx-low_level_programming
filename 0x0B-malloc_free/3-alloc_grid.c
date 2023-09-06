2479c87ef1780d0d35fcbc89caacdb9da85278ff
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-05 18:19:22 -0700

    fixed it?

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (!(width > 0 && height > 0))
 		return (NULL);
 
-	ptr = malloc(height * 8);
+	ptr = malloc(height * sizeof(int *));
 	if (ptr == NULL)
 	{
 		free(ptr);
@@ -24,15 +24,16 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		ptr[i] = malloc(width * 4);
+		ptr[i] = malloc(width * sizeof(int));
 		if (ptr[i] == NULL)
 		{
-			i--;
 			while (i >= 0)
 			{
 				free(ptr[i]);
 				i--;
 			}
+			free(ptr);
+
 			return (NULL);
 		}
 	}
