e8847eb1e3db11cd601287e91651f39ed25cb98f
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-07 15:55:30 -0700

    idk what im doing at this point

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
-* counter - counts string chars
-* @s: pointer to the string
-*
-* Return: char count of the string
-*/
+ * counter - counts string chars
+ * @s: pointer to the string
+ *
+ * Return: char count of the string
+ */
 unsigned int counter(char *s)
 {
 	unsigned int c = 0;
@@ -26,44 +26,34 @@ unsigned int counter(char *s)
 }
 
 /**
-* *string_nconcat - concatenates two strings
-* @s1: pointer to the first string
-* @s2: pointer to the second string
-* @n: number of chars from s2 to be concatenated
-*
-* Return: pointer to the concatenated string, or null
-*/
+ * *string_nconcat - concatenates two strings
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string
+ * @n: number of chars from s2 to be concatenated
+ *
+ * Return: pointer to the concatenated string, or null
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int c1, c2, i = 0;
+	unsigned int c1, i = 0;
 	char *nptr;
 
 	c1 = counter(s1);
-	c2 = counter(s2);
-	nptr = malloc(c1 + c2 + 1);
+	if (counter(s2) < n)
+		n = counter(s2);
+
+	nptr = malloc(c1 + n + 1);
 
 	if (nptr == 0)
 		return (NULL);
 
-	if (s1 != NULL)
-	{
-		for (i = 0; i < c1; i++)
-			nptr[i] = s1[i];
-	}
+	for (i = 0; i < c1; i++)
+		nptr[i] = s1[i];
 
-	if (s2 != NULL)
-	{
-		for (i = 0; i < n && i < c2; i++)
-		{
-			nptr[c1 + i] = s2[i];
+	for (i = 0; i < n; i++)
+		nptr[c1 + i] = s2[i];
 
-		}
-	}
-
-	if (n >= c2)
-		nptr[c1 + c2 + 1] = '\0';
-	else
-		nptr[c1 + n + 1] = '\0';
+	nptr[c1 + n] = '\0';
 
 	return (nptr);
 }
