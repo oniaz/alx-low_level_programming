592e5e15e561be46029340821c9f3dd65447a961
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-12 01:07:12 +0300

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef TASK0
+#define TASK0
+
+/**
+ * struct dog - struct for dog (name, age, owner)
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
