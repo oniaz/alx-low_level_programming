5df6c26b78b46db9fa0a090115e4cbf5f4cc250b
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-12 03:18:34 +0300

    closed open parenthesis

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,7 +11,7 @@ void print_dog(struct dog *d)
 	{
 		if (!(*d).name)
 			(*d).name = "(nil)";
-		if (!(*d).owner
+		if (!(*d).owner)
 			d->owner = "(nil)";
 
 		printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, d->owner);
