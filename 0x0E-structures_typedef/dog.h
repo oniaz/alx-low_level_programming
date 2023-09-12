0cca7ee341a62cb3222810931dd51726904c024f
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-11 19:52:57 -0700

    task 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,3 @@
-#ifndef TASK0
 #define TASK0
 
 /**
@@ -22,5 +21,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
