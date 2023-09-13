92a47693d6edf885d50d12ff7d2900da87c3eaa3
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-13 09:08:56 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_name - prints a name
+ * @name: pointer to the name to be printed
+ * @f: pointer to the function to be used
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,6 @@
+#ifndef MAINH
+#define MAINH
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
