da8ea3706088265c95865820a45ea55e54c2613b
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-14 14:28:43 -0700

    task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums passed arguments
+ * @n: number of arguments to be summed
+ * @...: arguments to be summed
+ *
+ * Return: sum of arguments, or 0 if n = 0
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum;
+
+	sum = 0;
+
+	if (n != 0)
+	{
+		va_list tots;
+
+		va_start(tots, n);
+
+		for (i = 0; i < n; i++)
+			sum += va_arg(tots, int);
+
+	}
+
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARH
+#define VARH
+
+#include <stdlib.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+
+#endif
