c42dc1f0193ef1f467d8c8d0fcd9d770f7f2ccbd
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-21 19:55:35 -0700

    fr though i didnt copy it this time

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,9 @@
-section .data
-    format db "Hello, Holberton", 10, 0
-
-section .text
 global main
-
 extern printf
-
 main:
-    push rdi
-    lea rdi, [rel format]
-    call printf
-    add rsp, 8
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton`, 10, 0
