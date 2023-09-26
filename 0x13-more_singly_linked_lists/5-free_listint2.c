2e689649901af7d3e178b7a55ee658570dd33e57
oniaz <32391049+oniaz@users.noreply.github.com>
2023-09-26 04:49:59 -0700

    task 5 - same as prev task but with double pointers

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Frees a list_t list.
+ * @head: pointer to the pointer to the head node of the list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *nextnode;
+
+	while (*head)
+	{
+		nextnode = (*head)->next;
+		free(*head);
+		(*head) = nextnode;
+	}
+}
