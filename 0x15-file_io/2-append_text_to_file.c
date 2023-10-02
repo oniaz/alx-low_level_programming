c702d57882d958ec11c400e9edccaad2c8700c8f
“oniaz” <32391049+oniaz@users.noreply.github.com>
2023-10-02 16:45:09 -0700

    dont know what's wrong

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,11 +4,11 @@
  * append_text_to_file - appends text at the end of a file.
  * @filename: poitner to a string containing the file path.
  * @text_content: text to be appended to the file.
+ * 
  * Return: 1 on success, -1 on failure.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-
 	ssize_t fd, l = 0;
 
 	if (!filename)
