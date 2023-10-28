30f89fafc93c3c6e0efc58ca184468e7e2d90674
oniaz <32391049+oniaz@users.noreply.github.com>
2023-10-28 15:04:06 +0200

    will i lose points if i edit now?
    I already passed all the checks but 3 before the first deadline. If I do the rest now will I get 50% of all the checks or just the new ones?

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,39 +1,61 @@
 #include "main.h"
 
 /**
- * print_number - cprints an integer.
+ * unflipNprint - flips a given number then prints it
+ * @flippedNum: the number
+ * @digitCount: number of digints in the number
+ */
+void unflipNprint(int flippedNum, int digitCount)
+{
+	int digit = 0;
+
+	while (digitCount)
+	{
+		digit = flippedNum % 10;
+		if (digit < 0)
+			digit *= -1;
+		_putchar(digit + '0');
+		flippedNum = flippedNum / 10;
+		digitCount--;
+	}
+}
+/**
+ * print_number - prints a given integer.
  * @n: the integer to be printed
  */
 void print_number(int n)
 {
-	unsigned int digit = 0, num = n, num2 = 0;
+	int num = n, negativeFlag = 0, digitCount, digit = 0, flippedNum = 0, i;
 
-	if (n < 0)
-	{
-		_putchar('-');
-		num *= -1;
-	}
-
-	if (num == 0)
+	if (n == 0)
 	{
 		_putchar(0 + '0');
+		return;
 	}
 
-	else
+	if (n < 0)
+		negativeFlag = 1;
+
+	for (digitCount = 0; num && digitCount < 9; num /= 10)
+		digitCount++;
+	num = n;
+
+	for (i = 0; i < digitCount; i++)
 	{
-		while (num > 0)
-		{
-			digit = num % 10;
-			num2 *= 10;
-			num2 += digit;
-			num = num / 10;
-		}
-
-		while (num2 > 0)
-		{
-			digit = num2 % 10;
-			_putchar(digit + '0');
-			num2 = num2 / 10;
-		}
+		digit = num % 10;
+		flippedNum *= 10;
+		flippedNum += digit;
+		num = num / 10;
 	}
+
+	if (negativeFlag)
+		_putchar('-');
+
+	if (num)
+	{
+		if (num < 0)
+			num *= -1;
+		_putchar(num + '0');
+	}
+	unflipNprint(flippedNum, digitCount);
 }
