075a702b95ab1f281fe73b8c9c6e806fbe20d864
“oniaz” <32391049+oniaz@users.noreply.github.com>
2023-11-13 09:57:14 -0800

    them files for the task

---
+++ 0x18-dynamic_libraries/op/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mods two numbers
+ * @a: first number
+ * @b: second number
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
