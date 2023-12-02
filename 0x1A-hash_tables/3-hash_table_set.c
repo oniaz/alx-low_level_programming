9a9c5e1e618200c4eed008f719bd32273fe3f77a
oniaz <32391049+oniaz@users.noreply.github.com>
2023-12-02 05:30:07 +0200

    man there's no sha8af at all. im doing this bel3afya fr

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,24 +11,43 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *node;
+	char *valueCpy = NULL;
+	hash_node_t *ptr;
 
-
-	if (key == NULL || key[0] == '\0' || value == NULL || ht == NULL)
+	if (key == NULL || *key == '\0' || value == NULL || ht == NULL)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
+	ptr = ht->array[index];
+	while (ptr)
+	{
+		if (strcmp(key, ptr->value))
+		{valueCpy = malloc(sizeof(strlen(value)));
+			strcpy(valueCpy, value);
+			ptr->value = valueCpy;
+			free(valueCpy);
+			return (1);
+		}
+		ptr = ptr->next;
+	}
+	
+	
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
-	strcpy(node->key, key);
-	strcpy(node->value, value);
+
+	node->key = (char*) key;
+	
+	valueCpy = malloc(sizeof(strlen(value)));
+	strcpy(valueCpy, value);
+	node->value = valueCpy;
+	free(valueCpy);
+
 	node->next = NULL;
 
 	if (ht->array[index] != NULL)
 		node->next = ht->array[index];
 
 	ht->array[index] = node;
-
 	return (1);
 }
